03275291a50ccf381be28d6ed577e03166cb2e66
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2022-10-03 17:33:46 +0200

    Licence blurb

--- prngperf/src/main/java/test/BenchmarkSplittableRandom.java
+++ prngperf/src/main/java/test/BenchmarkSplittableRandom.java
@@ -1,3 +1,22 @@
+/*
+ * DSI utilities
+ *
+ * Copyright (C) 2022 Sebastiano Vigna
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the GNU Lesser General Public License v2.1 or later,
+ * which is available at
+ * http://www.gnu.org/licenses/old-licenses/lgpl-2.1-standalone.html,
+ * or the Apache Software License 2.0, which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR Apache-2.0
+ */
+
 package test;
 
 import java.util.concurrent.TimeUnit;
